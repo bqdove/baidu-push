0d392537cb27edfc04892801f5914facca42b974
twilroad <269044570@qq.com>
2017-04-10 19:06:29 +0800

    add injection for extension

---
+++ resources/mixes/administration/src/helpers/injection.js
@@ -0,0 +1,21 @@
+import {
+    mixinRouter,
+    mixinSidebar,
+} from '../mixes/injection';
+
+const injection = {};
+
+function install(instance) {
+    Object.assign(injection, {
+        http: instance.http,
+        loading: instance.loading,
+        message: instance.message,
+        sidebar: instance.sidebar,
+    });
+    mixinRouter(instance);
+    mixinSidebar(instance);
+}
+
+export default Object.assign(injection, {
+    install,
+});
